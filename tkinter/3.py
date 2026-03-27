077dcfe39750f6bd6373f07f4d1413d3aefd7fe8
Deepanshishukla2009 <shuklashiri154@gmail.com>
2026-03-27 13:34:39 +0530

    daily

--- tkinter/3.py
+++ tkinter/3.py
@@ -1,5 +1,5 @@
 from tkinter import *
 root = Tk()
-myButton = Button(root, text="click", padx=20 ,pady=20)
-myButton.pack()
+myButton = Button(root, text="click", padx=20,pady=20).grid(row=1, column=20)
+
 root.mainloop()
